daa2038fe1193bb9b9b33e7a78bb306a62f9d05a
Miikis <28997062+Miikis@users.noreply.github.com>
2019-10-18 08:48:55 -0700

    Finish ring_buffer
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,19 @@
 class RingBuffer:
+    """
+    A ring buffer is a non-growable buffer with a fixed size.
+    When the ring buffer is full and a new element is inserted,
+    the oldest element in the ring buffer is overwritten with the newest element. 
+    """
+
     def __init__(self, capacity):
         self.capacity = capacity
         self.current = 0
         self.storage = [None]*capacity
 
     def append(self, item):
-        pass
+        self.storage[self.current] = item
+        self.current = (self.current + 1) % self.capacity
+        return self
 
     def get(self):
-        pass
+        return [item for item in self.storage if item != None]
